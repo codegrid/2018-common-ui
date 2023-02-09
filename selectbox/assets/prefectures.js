9af3a9e5cbe34eade71a3cc080b44f92b1e05171
matori <pnr.matori@gmail.com>
2023-02-09 18:58:29 +0900

    連載の第17回用デモを追加

---
+++ selectbox/assets/prefectures.js
@@ -0,0 +1,332 @@
+export const prefectures = [
+  {
+    "ja": "北海道",
+    "en": "Hokkaido",
+    "items": [
+      {
+        "id": 1,
+        "ja": "北海道",
+        "en": "Hokkaido",
+        "value": "hokkaido"
+      }
+    ]
+  },
+  {
+    "ja": "東北",
+    "en": "Tohoku",
+    "items": [
+      {
+        "id": 2,
+        "ja": "青森県",
+        "en": "Aomori",
+        "value": "aomori"
+      },
+      {
+        "id": 3,
+        "ja": "岩手県",
+        "en": "Iwate",
+        "value": "iwate"
+      },
+      {
+        "id": 4,
+        "ja": "宮城県",
+        "en": "Miyagi",
+        "value": "miyagi"
+      },
+      {
+        "id": 5,
+        "ja": "秋田県",
+        "en": "Akita",
+        "value": "akita"
+      },
+      {
+        "id": 6,
+        "ja": "山形県",
+        "en": "Yamagata",
+        "value": "yamagata"
+      },
+      {
+        "id": 7,
+        "ja": "福島県",
+        "en": "Fukushima",
+        "value": "fukushima"
+      }
+    ]
+  },
+  {
+    "ja": "関東",
+    "en": "Kanto",
+    "items": [
+      {
+        "id": 8,
+        "ja": "茨城県",
+        "en": "Ibaraki",
+        "value": "ibaraki"
+      },
+      {
+        "id": 9,
+        "ja": "栃木県",
+        "en": "Tochigi",
+        "value": "tochigi"
+      },
+      {
+        "id": 10,
+        "ja": "群馬県",
+        "en": "Gunma",
+        "value": "gunma"
+      },
+      {
+        "id": 11,
+        "ja": "埼玉県",
+        "en": "Saitama",
+        "value": "saitama"
+      },
+      {
+        "id": 12,
+        "ja": "千葉県",
+        "en": "Chiba",
+        "value": "chiba"
+      },
+      {
+        "id": 13,
+        "ja": "東京都",
+        "en": "Tokyo",
+        "value": "tokyo"
+      },
+      {
+        "id": 14,
+        "ja": "神奈川県",
+        "en": "Kanagawa",
+        "value": "kanagawa"
+      }
+    ]
+  },
+  {
+    "ja": "中部",
+    "en": "Chubu",
+    "items": [
+      {
+        "id": 15,
+        "ja": "新潟県",
+        "en": "Niigata",
+        "value": "niigata"
+      },
+      {
+        "id": 16,
+        "ja": "富山県",
+        "en": "Toyama",
+        "value": "toyama"
+      },
+      {
+        "id": 17,
+        "ja": "石川県",
+        "en": "Ishikawa",
+        "value": "ishikawa"
+      },
+      {
+        "id": 18,
+        "ja": "福井県",
+        "en": "Fukui",
+        "value": "fukui"
+      },
+      {
+        "id": 19,
+        "ja": "山梨県",
+        "en": "Yamanashi",
+        "value": "yamanashi"
+      },
+      {
+        "id": 20,
+        "ja": "長野県",
+        "en": "Nagano",
+        "value": "nagano"
+      },
+      {
+        "id": 21,
+        "ja": "岐阜県",
+        "en": "Gifu",
+        "value": "gifu"
+      },
+      {
+        "id": 22,
+        "ja": "静岡県",
+        "en": "Shizuoka",
+        "value": "shizuoka"
+      },
+      {
+        "id": 23,
+        "ja": "愛知県",
+        "en": "Aichi",
+        "value": "aichi"
+      }
+    ]
+  },
+  {
+    "ja": "近畿",
+    "en": "Kinki",
+    "items": [
+      {
+        "id": 24,
+        "ja": "三重県",
+        "en": "Mie",
+        "value": "mie"
+      },
+      {
+        "id": 25,
+        "ja": "滋賀県",
+        "en": "Shiga",
+        "value": "shiga"
+      },
+      {
+        "id": 26,
+        "ja": "京都府",
+        "en": "Kyoto",
+        "value": "kyoto"
+      },
+      {
+        "id": 27,
+        "ja": "大阪府",
+        "en": "Osaka",
+        "value": "osaka"
+      },
+      {
+        "id": 28,
+        "ja": "兵庫県",
+        "en": "Hyogo",
+        "value": "hyogo"
+      },
+      {
+        "id": 29,
+        "ja": "奈良県",
+        "en": "Nara",
+        "value": "nara"
+      },
+      {
+        "id": 30,
+        "ja": "和歌山県",
+        "en": "Wakayama",
+        "value": "wakayama"
+      }
+    ]
+  },
+  {
+    "ja": "中国",
+    "en": "Chugoku",
+    "items": [
+      {
+        "id": 31,
+        "ja": "鳥取県",
+        "en": "Tottori",
+        "value": "tottori"
+      },
+      {
+        "id": 32,
+        "ja": "島根県",
+        "en": "Shimane",
+        "value": "shimane"
+      },
+      {
+        "id": 33,
+        "ja": "岡山県",
+        "en": "Okayama",
+        "value": "okayama"
+      },
+      {
+        "id": 34,
+        "ja": "広島県",
+        "en": "Hiroshima",
+        "value": "hiroshima"
+      },
+      {
+        "id": 35,
+        "ja": "山口県",
+        "en": "Yamaguchi",
+        "value": "yamaguchi"
+      }
+    ]
+  },
+  {
+    "ja": "四国",
+    "en": "Shikoku",
+    "items": [
+      {
+        "id": 36,
+        "ja": "徳島県",
+        "en": "Tokushima",
+        "value": "tokushima"
+      },
+      {
+        "id": 37,
+        "ja": "香川県",
+        "en": "Kagawa",
+        "value": "kagawa"
+      },
+      {
+        "id": 38,
+        "ja": "愛媛県",
+        "en": "Ehime",
+        "value": "ehime"
+      },
+      {
+        "id": 39,
+        "ja": "高知県",
+        "en": "Kochi",
+        "value": "kochi"
+      }
+    ]
+  },
+  {
+    "ja": "九州",
+    "en": "Kyushu",
+    "items": [
+      {
+        "id": 40,
+        "ja": "福岡県",
+        "en": "Fukuoka",
+        "value": "fukuoka"
+      },
+      {
+        "id": 41,
+        "ja": "佐賀県",
+        "en": "Saga",
+        "value": "saga"
+      },
+      {
+        "id": 42,
+        "ja": "長崎県",
+        "en": "Nagasaki",
+        "value": "nagasaki"
+      },
+      {
+        "id": 43,
+        "ja": "熊本県",
+        "en": "Kumamoto",
+        "value": "kumamoto"
+      },
+      {
+        "id": 44,
+        "ja": "大分県",
+        "en": "Oita",
+        "value": "oita"
+      },
+      {
+        "id": 45,
+        "ja": "宮崎県",
+        "en": "Miyazaki",
+        "value": "miyazaki"
+      },
+      {
+        "id": 46,
+        "ja": "鹿児島県",
+        "en": "Kagoshima",
+        "value": "kagoshima"
+      },
+      {
+        "id": 47,
+        "ja": "沖縄県",
+        "en": "Okinawa",
+        "value": "okinawa"
+      }
+    ]
+  }
+];
